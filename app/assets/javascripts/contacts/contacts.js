1d5ad45f55c53037930b0fc816ab93c2a6ba5995
Alex Savchenko <osavchenko@velti.com>
2015-04-14 18:46:35 +0300

    Added breadcrumbs.

--- app/assets/javascripts/contacts/contacts.js
+++ app/assets/javascripts/contacts/contacts.js
@@ -5,13 +5,21 @@ angular.module('appContacts', ['appCommon', 'spinner']).config([
     $stateProvider.state('contacts', {
       url: '/contacts',
       abstract: true,
-      templateUrl: 'assets/templates/contacts/views/contacts.html'
+      templateUrl: 'assets/templates/contacts/views/contacts.html',
+      data: {
+        breadcrumbProxy: 'contacts.list'
+      }
     });
 
     $stateProvider.state('contacts.list', {
       url: '/list',
-
+      data: {
+        displayName: 'Contact list'
+      },
       views: {
+        'breadcrumbs': {
+          templateUrl: 'assets/templates/common/breadcrumbs.tpl.html'
+        },
         'list': {
           templateUrl: 'assets/templates/contacts/views/contact_list.html',
           controller: 'ContactList'
@@ -21,8 +29,13 @@ angular.module('appContacts', ['appCommon', 'spinner']).config([
 
     $stateProvider.state('contacts.create', {
       url: '/create',
-
+      data: {
+        displayName: 'Create new contact'
+      },
       views: {
+        'breadcrumbs@contacts.create': {
+          templateUrl: 'assets/templates/common/breadcrumbs.tpl.html'
+        },
         '@': {
           templateUrl: 'assets/templates/contacts/views/contact_form.html',
           controller: 'ContactForm'
@@ -32,8 +45,13 @@ angular.module('appContacts', ['appCommon', 'spinner']).config([
 
     $stateProvider.state('contacts.edit', {
       url: '/edit/:id',
-
+      data: {
+        displayName: 'Edit contact'
+      },
       views: {
+        'breadcrumbs@contacts.edit': {
+          templateUrl: 'assets/templates/common/breadcrumbs.tpl.html'
+        },
         '@': {
           templateUrl: 'assets/templates/contacts/views/contact_form.html',
           controller: 'ContactForm'
--- app/assets/javascripts/products/products.js
+++ app/assets/javascripts/products/products.js
@@ -1,16 +1,25 @@
-angular.module('products', ['appCommon']).config([
+angular.module('products', ['appCommon', 'angularUtils.directives.uiBreadcrumbs']).config([
   '$stateProvider', function($stateProvider) {
     'use strict';
 
     $stateProvider.state('products', {
       url: '/products',
       abstract: true,
-      templateUrl: 'assets/templates/products/products.html'
+      templateUrl: 'assets/templates/products/products.html',
+      data: {
+        breadcrumbProxy: 'products.list'
+      }
     });
 
     $stateProvider.state('products.list', {
       url: '/list',
+      data: {
+        displayName: 'Product list'
+      },
       views: {
+        'breadcrumbs': {
+          templateUrl: 'assets/templates/common/breadcrumbs.tpl.html'
+        },
         'sidebar': {
           templateUrl: 'assets/templates/products/product_sidebar.html',
           controller: 'productSidebar'
@@ -24,7 +33,17 @@ angular.module('products', ['appCommon']).config([
 
     $stateProvider.state('products.view', {
       url: '/view/:id',
+      data: {
+        displayName: 'Product view'
+      },
       views: {
+        'breadcrumbs@products.view': {
+          templateUrl: 'assets/templates/common/breadcrumbs.tpl.html'
+        },
+        'sidebar@products.view': {
+          templateUrl: 'assets/templates/products/product_sidebar.html',
+          controller: 'productSidebar'
+        },
         '@': {
           templateUrl: 'assets/templates/products/product_view.html',
           controller: 'productView'
